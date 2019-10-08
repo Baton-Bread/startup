ade0020eb09ef355ad508c27ef83f4bb794cdf27
Baton-Bread <kokorobere1337@gmail.com>
2019-10-08 14:38:38 +0700

    lol

--- Lesson4/1.py
+++ Lesson4/1.py
@@ -13,6 +13,42 @@ def get_course(chr_code, count):
         if x.charcode.text == chr_code:
             nominal = int(x.nominal.text)
             value = float(x.value.text.replace(",", "."))
-            print(f"За {nominal*count} {chr_code} дают {nominal * value * count} дере")
+            print(f"За {nominal*count} {chr_code} дают {nominal * value * count} деревянных.")
 
 def calc():
+    print("Добро пожаловать в Базовый консольный калькулятор Python!")
+    print("Чтобы проводить операции с числами вам нужно ввести команду операции.")
+    print("Эта версия калькулятора поддерживает только 5 команд: *,/,-,+,%.")
+    print("* - умножение, / - деление, - -- вычитание,% - выделить остаток из деления, и + - сложение соответственно.")
+    print("Удачи!")
+
+
+    command = input("Введите команду: ")
+    num1 = float(input("Введите 1-e число: "))
+    num2 = float(input("Введите 2-e число: "))
+
+
+    if command == "+":
+        print(num1 + num2)
+    elif command == "*":
+        print(num1 * num2)
+    elif command == "/":
+        print(num1 / num2)
+    elif command == "-":
+        print(num1 - num2)
+    elif command == "%":
+        print(num1 % num2)
+    else:
+        print("Неверная команда!")
+
+
+lol = input("Какую программу хотите использовать?(Введите: 'Калькулятор' или 'Конвертер': ")
+if lol == "Калькулятор":
+    print(calc())
+elif lol == "Конвертер":
+    charcode = str(input("Введите CharCode: "))
+    how_much = int(input("Сколько вам нужно валюты?: "))
+    print(get_course(charcode, how_much))
+else:
+    print("Такой программы нет или неправильно введена команда.")
+
