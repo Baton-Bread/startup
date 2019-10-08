c463b3625fcd74498996084f51a845294dfabfac
Baton-Bread <kokorobere1337@gmail.com>
2019-10-08 13:57:11 +0700

    lol

--- dz/main.py
+++ dz/main.py
@@ -1,15 +1,17 @@
 import requests
 import bs4
-
+ 
 response = requests.get("https://www.cbr.ru/scripts/XML_daily.asp")
+ 
 soup = bs4.BeautifulSoup(response.content, "lxml")
-
-chr_code = 'USD'
-
+ 
+chr_code = "CNY"
+count = 10
+ 
 valutes = soup.find_all("valute")
-
+ 
 for x in valutes:
     if x.charcode.text == chr_code:
-        nominal - int(x.nominal.text)
-        value = float(x.value.text.replace)
-        print(f"За...")
+        nominal = int(x.nominal.text)
+        value = float(x.value.text.replace(",", "."))
+        print(f"За {nominal*count} {chr_code} дают {nominal * value * count}руб")
