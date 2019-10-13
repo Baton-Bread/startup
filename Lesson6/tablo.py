37ddf25b25f7ca30af5bd18061d5cae3500d0934
Baton-Bread <kokorobere1337@gmail.com>
2019-10-13 19:06:42 +0700

    dasd

--- Lesson6/tablo.py
+++ Lesson6/tablo.py
@@ -13,8 +13,12 @@ logo.place(x=0, y=0)
 lab = tkinter.Label(text="Курс валют \n MAXIMUM банк", fg="black", font="Arial 22")
 lab.place(x=150, y=25)
 
+usd_course = tkinter.Label(text=f"$ {get_course('R01235')}", font="Arial 16")
+usd_course.place(x=90, y=150)
+
+eur_course = tkinter.Label(text=f"€ {get_course('R01239')}", font="Arial 16")
+eur_course.place(x=90, y=190) 
+
 
 
 window.mainloop()
-
-print(get_course("R01235"))
